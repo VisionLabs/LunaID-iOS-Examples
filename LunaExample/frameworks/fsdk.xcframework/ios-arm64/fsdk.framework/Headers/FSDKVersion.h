3a3c1d6e7dd110204dc63f48f80f5b274d8c4ede
Michael <m.terekhov@visionlabs.ru>
2024-07-04 10:45:18 +0300

    removing LunaID SDK binaries to avoid LFS problems

--- LunaExample/frameworks/fsdk.xcframework/ios-arm64/fsdk.framework/Headers/FSDKVersion.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// clang-format off
-
-#define FSDK_VERSION "5.21.1"
-#define FSDK_VERSION_MAJOR 5
-#define FSDK_VERSION_MINOR 21
-#define FSDK_VERSION_PATCH 1
-#define FSDK_VERSION_HASH "45572a248dfd0fbf16c9610abc086e61fb862010"
--- LunaExample/frameworks/fsdk.xcframework/ios-arm64/fsdk.framework/Headers/Optional.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <fsdk/vlc/optional.h>
-
-namespace fsdk {
-	template <typename T>
-	using Optional = vlc::optional<T>;
-}
